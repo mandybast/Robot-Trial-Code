39ae7a83ffb901d80347e2cf18c7067163c9a7ac
Amanda Bast <mandygal0009@yahoo.com>
2015-04-25 16:05:26 -0500

    claw tweeking
    just angles and such

--- ClawTest/ClawTest2/ClawTest2.ino
+++ ClawTest/ClawTest2/ClawTest2.ino
@@ -3,7 +3,7 @@
 Servo GripMotor;  // create servo object to control a servo 
                 // twelve servo objects can be created on most boards
  
-int pos = 0;    // variable to store the servo position 
+int pos = 35;    // variable to store the servo position 
  
 void setup() 
 { 
@@ -12,12 +12,12 @@ void setup()
  
 void loop() 
 { 
-  for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+  for(pos = 35; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     GripMotor.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   } 
-  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+  for(pos = 180; pos>=35; pos-=1)     // goes from 180 degrees to 0 degrees 
   {                                
     GripMotor.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
