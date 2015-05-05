b91ab5e052779b92a07cb59ac2b69989ca01355c
Amanda Bast <mandygal0009@yahoo.com>
2015-05-04 19:57:06 -0500

    new claw stuffizzle
    New claw test code

--- ClawTest/ClawTest2/ClawTest2.ino
+++ ClawTest/ClawTest2/ClawTest2.ino
@@ -1,13 +1,14 @@
 #include <Servo.h> 
  
-Servo GripMotor;  // create servo object to control a servo 
-                // twelve servo objects can be created on most boards
+Servo GripMotor;  // create servo objects to control a servo 
+Servo WristMotor;            
  
 int pos = 35;    // variable to store the servo position 
- 
+
 void setup() 
 { 
-  GripMotor.attach(10);  // attaches the servo on pin 9 to the servo object 
+  GripMotor.attach(10);  // attaches the servo on pin 10 to the servo object 
+  WristMotor.attach(9);  // attaches the servo on pin 9 to the servo object 
 } 
  
 void loop() 
@@ -15,11 +16,15 @@ void loop()
   for(pos = 35; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     GripMotor.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
+    delay(15);       // waits 15ms for the servo to reach the position
+    WristMotor.write(pos);  
+    delay(15);       // waits 15ms for the servo to reach the position
   } 
   for(pos = 180; pos>=35; pos-=1)     // goes from 180 degrees to 0 degrees 
   {                                
     GripMotor.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
+    WristMotor.write(pos);  
+    delay(15);       // waits 15ms for the servo to reach the position
   } 
 } 
